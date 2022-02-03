65eda1aee45dfc167b6b3f40b4e0f24472853662
rioxdev <rioxdev@gmail.com>
2022-02-03 18:42:58 +0100

    model validation

---
+++ ApressMvc/Models/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApressMvc.Models
+{
+    public class Appointment
+    {
+        public string ClientName { get; set; }
+        [UIHint("Date")]
+        public DateTime Date { get; set; }
+        public bool TemrsAccepted { get; set; }
+            
+    }
+}
