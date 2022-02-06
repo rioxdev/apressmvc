249619560948bc57d0f7c834ea0b1f22c9acab6f
henrio randriasoa <rioxdev@gmail.com>
2022-02-06 17:38:07 +0100

    validation

--- ApressMvc/Controllers/AppointmentController.cs
+++ ApressMvc/Controllers/AppointmentController.cs
@@ -14,7 +14,28 @@ namespace ApressMvc.Controllers
         [HttpPost]
         public IActionResult MakeBooking(Appointment model)
         {
-            return View("Completed", model);
+
+            if (String.IsNullOrEmpty(model.ClientName))
+            {
+                ModelState.AddModelError(nameof(model.ClientName), "Please enter your name");
+            }
+
+            if (ModelState.GetValidationState("Date") == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Valid
+                && DateTime.Now > model.Date)
+            {
+                ModelState.AddModelError(nameof(model.Date), "Please enter a date in the future");
+            }
+
+            if (!model.TemrsAccepted)
+            {
+                ModelState.AddModelError(nameof(model.TemrsAccepted), "You must accept the terms");
+            }
+
+
+            if (ModelState.IsValid)
+               return  View("Completed", model);
+            else
+               return  RedirectToAction("Index");
         }
     }
 }
--- ApressMvc/Models/Appointment.cs
+++ ApressMvc/Models/Appointment.cs
@@ -5,9 +5,12 @@ namespace ApressMvc.Models
 {
     public class Appointment
     {
+        [Required]
+        [MaxLength(15)]
         public string ClientName { get; set; }
         [UIHint("Date")]
         public DateTime Date { get; set; }
+        [Required]
         public bool TemrsAccepted { get; set; }
             
     }
